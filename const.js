951da3789bec22d1c4e4a0c77bd0aeb9e24d3b1a
Andres felipe Sierra <asierra447@gmail.com>
2025-01-24 10:29:06 -0500

    cambios en const

--- const.js
+++ const.js
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
-export const PUBLICKEY = 'e95a746b0a6bcf528247b6455c1380b0';
-export const PRIVATKEY = 'ee3102c5cde55bf1bbe68e034f82bfc2204480ce';
+export const PUBLICKEY = '<TU_PUBLICKEY>';
+export const PRIVATKEY = '<TU_PRIVATEKEY>';;
 export const MAXRESULTPERPAGE = 6;
