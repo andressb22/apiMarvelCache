a561711260e35f51225b7168069debbfc8e15c2d
Andres felipe Sierra <asierra447@gmail.com>
2025-01-24 10:29:30 -0500

    eliminar coma

--- const.js
+++ const.js
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
 export const PUBLICKEY = '<TU_PUBLICKEY>';
-export const PRIVATKEY = '<TU_PRIVATEKEY>';;
+export const PRIVATKEY = '<TU_PRIVATEKEY>';
 export const MAXRESULTPERPAGE = 6;
